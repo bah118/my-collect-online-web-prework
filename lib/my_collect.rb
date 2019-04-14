0e0a90e0f37ceb3b3840ae20b4a5e206d20bf1a4
BA Hanlon <herbivore137@gmail.com>
2019-04-14 05:08:23 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,9 +2,9 @@ def my_collect(array)
   i = 0 
   new_array = []
   while i < array.length 
-    yield(array[i])
-    new_array << i 
+    yield(new_array << array[i])
+    #new_array << i 
     i = i + 1 
   end
- # new_array
+  new_array
 end
